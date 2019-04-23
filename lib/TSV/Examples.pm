6ec965a10e4897a511521e86332c7d513303a24f
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-23 14:26:35 +0700

    Initial commit

---
+++ lib/TSV/Examples.pm
@@ -0,0 +1,15 @@
+package TSV::Examples;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Example TSV files
+
+=head1 DESCRIPTION
+
+This distribution contains various examples of TSV (tab-separated values) files
+in its per-dist share dir.
+
+
+=head1 SEE ALSO
